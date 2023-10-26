c25d1eaaf7c8c1b83c31f2b12f068fe4e866d528
Francesco Rapetti <francescorapetti1998@gmail.com>
2023-10-26 16:15:57 +0200

    Implement discount system and ticket managment

--- main.js
+++ main.js
@@ -8,7 +8,65 @@
 // OUTPUT
 // Il prezzo finale va scritto in pagina in forma umana, quindi con massimo due decimali, per indicare centesimi sul prezzo.
 
+let kilometri;
+let eta;
+const prezzoAlKm = 0.21;
+let prezzo;
+let sconto;
+let output = '';
 
-const kilometri = prompt("Inserisci il numero di kilometri da percorrere");
-const eta = prompt("Inserisci la tua età");
+kilometri = promptManager(kilometri, "Il numero di kilometri da percorrere");
+eta = promptManager(eta, "la tua età");
+let prezzoPieno = (kilometri * prezzoAlKm);
+
+if (eta < 18) {
+    sconto = 20;
+    output += `Complimenti, hai diritto ad uno sconto del ${sconto}%: `;
+    prezzo = prezzoPieno - percentageCalculator(prezzoPieno, sconto);
+} else if (eta > 65) {
+    sconto = 40;
+    output += `Complimenti, hai diritto ad uno sconto del ${sconto}%: `;
+    prezzo = prezzoPieno - percentageCalculator(prezzoPieno, sconto);
+} else {
+    prezzo = prezzoPieno;
+}
+
+console.log("kilometri: "+kilometri);
+console.log("età: "+eta);
+console.log("prezzo pieno: "+prezzoPieno);
+console.log("prezzo scontato"+prezzo);
+
+prezzo = prezzo.toFixed(2);
+output += `Il costo del biglietto è di: ${prezzo} euro`;
+document.getElementById("message").innerHTML = output;
+
+function promptManager(input, inputName) {
+    while (true) {
+        input = prompt(`Inserisci ${inputName}`);
+        if (input <= 0 || !numberController(input)) {
+            alert(`Valore non accettato, riprova`);
+        }
+        else {
+            return parseInt(input);
+        }
+    }
+}
+
+// checks if there are char in the string
+function numberController(input) {
+    for (let index = 0; index < input.length; index++) {
+        if (isNaN(input[index])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// returns percentage
+function percentageCalculator(num, percentage) {
+    return (num / 100) * percentage;
+}
+
+
+
 
